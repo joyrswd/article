1f6835049a4d91a2ba7ca703b30a1ff50f1c8bd5
joyrswd <5625893+joyrswd@users.noreply.github.com>
2024-02-20 14:09:02 +0900

    テーブル作成

---
+++ database/migrations/2024_02_20_140224_create_attributes_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attributes', function (Blueprint $table) {
+            $table->id()->comment('属性ID');
+            $table->string('name')->unique()->comment('属性名');
+            $table->string('type')->index()->comment('属性種別');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attributes');
+    }
+};
