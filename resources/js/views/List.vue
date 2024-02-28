f09f2b1aac124ede61e7e1ef222f9c712492c565
joyrswd <5625893+joyrswd@users.noreply.github.com>
2024-02-28 12:42:11 +0900

    画像表示対応

--- resources/js/views/List.vue
+++ resources/js/views/List.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="row" v-if="items.length">
-        <div class="col-md-4 d-flex flex-column justify-content-between p-4" v-for="item in items">
-            <h4 class="align-self-start">{{ item.title }}</h4>
-            <p class="align-self-stretch">{{ item.content.substring(0, 150) }}...</p>
-            <p class="text-end align-self-end"><router-link :to="`${item._links.self.href}`" class="badge rounded-pill btn btn-info"
-                    role="button">read more &raquo;</router-link></p>
+        <div class="col-md-4 d-flex flex-column justify-content-between p-4 position-relative" v-for="item in items">
+            <div v-if=item._links.image.href class="backgroundImage">
+                <img :src="item._links.image.href" alt="image">
+            </div>
+            <h4 class="align-self-start"><span>{{ item.title }}</span></h4>
+            <p class="align-self-stretch"><span>{{ item.content.substring(0, 150) }}...</span></p>
+            <p class="text-end align-self-end"><router-link :to="`${item._links.self.href}`"
+                    class="badge rounded-pill btn btn-info" role="button">read more &raquo;</router-link></p>
         </div>
     </div>
     <div class="text-center my-5" v-else>
