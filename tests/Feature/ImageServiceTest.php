773340f8856ad43e23ec5982b61c7e1b615cdb04
joyr <swd@joyr.net>
2024-02-29 20:10:22 +0900

    ImagickRepositoryテスト追加

--- tests/Feature/ImageServiceTest.php
+++ tests/Feature/ImageServiceTest.php
@@ -6,8 +6,8 @@ use App\Services\ImageService;
 use App\Models\Image;
 use App\Models\Article;
 use App\Models\Author;
+use App\Repositories\ImagickRepository;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Mockery;
 
 class ImageServiceTest extends FeatureTestCase
 {
@@ -63,5 +63,26 @@ class ImageServiceTest extends FeatureTestCase
             'model_name' => 'model_name',
         ]);
     }
+
+    /**
+     * @test
+     */
+    public function put_正常(): void
+    {
+        $mock = mock(ImagickRepository::class);
+        $mock->shouldReceive('setRectImage')->once()->andReturn(1);
+        $mock->shouldReceive('setTextOnImage')->once();
+        $mock->shouldReceive('setImageByUrl')->once()->andReturn(2);
+        $mock->shouldReceive('minimize')->once();
+        $mock->shouldReceive('compositeOver')->once();
+        $mock->shouldReceive('save')->once();
+        $mock->shouldReceive('clear')->once();
+        $this->setPrivateProperty('imagickRepository', $mock, $this->service);
+        $url = 'http://test.com/example.png';
+        $path = $this->service->put($url, 'watermark');
+        $expected = '/var/www/article/public/tmp/test/20240101/' . md5($url) . '.png';
+        $this->assertEquals($expected, $path);
+    }
+
 
 }
